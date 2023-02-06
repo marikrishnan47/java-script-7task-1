74f41e41bdaf97819f2b2bef5e5129b8289f7e44
marikrishnan47 <krishmarish028@gmail.com>
2023-02-06 16:17:35 +0530

    flie

---
+++ java-5.js
@@ -0,0 +1,23 @@
+
+  
+    const users = [
+  {
+    first_name: 'Mike',
+    last_name: 'Sheridan',
+    age: 30
+  },
+  {
+    first_name: 'Tim',
+    last_name: 'Lee',
+    age: 45
+  },
+  {
+    first_name: 'John',
+    last_name: 'Carte',
+    age: 25
+  }
+];
+ let name=users.map(function(value){
+   return value.last_name;
+ });
+  console.log (name)
